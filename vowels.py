7526917e373fa50067368e11b8c741a2e801f519
Heer-3011 <223sbeit54009@vsitr.ac.in>
2026-01-03 10:04:44 +0530

    vowels added

---
+++ vowels.py
@@ -0,0 +1,6 @@
+ch=input("Enter a character=")
+
+if (ch=='a' or ch=='i' or ch=='o' or ch=='u' or ch=='e' or ch=='A' or ch=='E' or ch=='O' or ch=='U' or ch=='I'):
+    print(f"{ch} is Vowel")
+else:
+    print(f"{ch} is Consonent")
